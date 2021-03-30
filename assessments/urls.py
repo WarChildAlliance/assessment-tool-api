5fee54a385243c564420cc29baa1d41936dd008e
Matthis M <matthis.reliefapps@gmail.com>
2021-03-30 09:12:43 +0200

    Cleaned the urls and began permissions on assessmt
    Co-authored-by: hélène <lnj-picard@users.noreply.github.com>

--- assessments/urls.py
+++ assessments/urls.py
@@ -1,19 +1,7 @@
 from django.urls import include, path
-from rest_framework import routers
 
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'', views.AssessmentsViewSet)
-router.register(r'topics', views.AssessmentTopicsViewSet)
-router.register(r'questions', views.QuestionsViewSet)
-router.register(r'question_inputs', views.QuestionsInputViewSet)
-router.register(r'question_selects', views.QuestionsSelectViewSet)
-router.register(r'question_sorts', views.QuestionsSortViewSet)
-router.register(r'attachments', views.AttachmentsViewSet)
-router.register(r'select_options', views.SelectOptionsViewSet)
-router.register(r'sort_options', views.SortOptionsViewSet)
-# Not sure about what we should remove in all that
 
 assessments_list = views.AssessmentsViewSet.as_view({
     'get': 'list',
@@ -35,7 +23,6 @@ attachement_detail = views.AttachmentsViewSet.as_view({
 
 
 urlpatterns = [
-    #path('', include(router.urls)),
     path('', assessments_list, name='assessments-list'),
     path('<int:pk>/', assessments_detail, name='assessments-detail'),
     path('questions/', questions_list, name='questions-list'),
--- assessments/views/assessments.py
+++ assessments/views/assessments.py
@@ -4,17 +4,40 @@ from rest_framework.response import Response
 from ..models import Assessment, AssessmentTopic
 from ..serializers import AssessmentSerializer, AssessmentTopicSerializer
 
+from users.models import User
+
+
 
 class AssessmentsViewSet(ModelViewSet):
     """
     Assessments viewset.
     """
 
-    queryset = Assessment.objects.all()
+    #queryset = Assessment.objects.all()
     serializer_class = AssessmentSerializer
     filterset_fields = ['grade', 'private', 'country', 'language', 'subject']
     search_fields = ['title', 'subject']
 
+    def get_queryset(self):
+        """
+        Queryset to get allowed assessments
+        """
+        if self.action != 'list':
+            return Assessment.objects.all()
+        # Deleting this should only prevent the students to retrieve a specifi assessment, but it prevents the supervisor too, why ?
+
+        user = self.request.user
+        if user.is_supervisor():
+            return Assessment.objects.all()
+      
+        return Assessment.objects.filter(private=False)
+
+    def retrieve(self, request, pk=None):
+
+        responseAssessment = self.get_queryset().filter(pk=pk)
+        serializer = AssessmentSerializer(responseAssessment, many=True)
+
+        return Response(serializer.data)
 
 class AssessmentTopicsViewSet(ModelViewSet):
     """
