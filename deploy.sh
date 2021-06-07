7b6af66972c4438d8bd7239db6182c0da734e61e
Theresa <theresa.oort@gmail.com>
2021-06-07 07:47:06 +0200

    Deploy test 2

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ OUT=.
 CONNECTION=reliefapps@92.243.25.191
 
 set -e
-echo -e "Synchronizing files updated..."
-rsync -e "ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=yes" --omit-dir-times -avzr --delete --exclude-from '.gitignore' $OUT/* $CONNECTION:$REMOTE_PATH
+echo -e "Synchronizing files..."
+rsync -e "ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=yes" -avzr --delete --exclude-from '.gitignore' $OUT/* $CONNECTION:$REMOTE_PATH
 #CMD="'""cd $REMOTE_PATH && mv docker-compose.yml.dist docker-compose.yml""'" TODO add when we have dist
 #ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "'"$CMD"'"
 
