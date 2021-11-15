3f280b19c960161ea58dfab51c2e4eb758d11447
Theresa Tobollik <theresa.oort@gmail.com>
2021-11-15 11:56:16 +0100

    adding identifiers to answers

--- answers/admin.py
+++ answers/admin.py
@@ -4,9 +4,26 @@ from .models import (Answer, AnswerInput, AnswerNumberLine, AnswerSelect,
                      AnswerSession, AnswerSort, AssessmentTopicAnswer)
 
 
+class AnswerSelectAnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'topic_answer', 'question_identifier', 'valid')
+    search_fields = ('id',)
+
+    def question_identifier(self, obj):
+        return obj.question.identifier
+
+class AnswerNumberlineAnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'topic_answer', 'question_identifier', 'valid')
+    search_fields = ('id',)
+
+    def question_identifier(self, obj):
+        return obj.question.identifier
+
+
+
+
 admin.site.register(AnswerSession, admin.ModelAdmin)
 admin.site.register(AnswerInput, admin.ModelAdmin)
-admin.site.register(AnswerNumberLine, admin.ModelAdmin)
-admin.site.register(AnswerSelect, admin.ModelAdmin)
+admin.site.register(AnswerNumberLine, AnswerNumberlineAnswerAdmin)
+admin.site.register(AnswerSelect, AnswerSelectAnswerAdmin)
 admin.site.register(AnswerSort, admin.ModelAdmin)
 admin.site.register(AssessmentTopicAnswer, admin.ModelAdmin)
