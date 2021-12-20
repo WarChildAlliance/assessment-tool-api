49071685688acd6d453c69a71d97a15e09fb8efb
Theresa Tobollik <theresa.oort@gmail.com>
2021-12-20 15:17:35 +0100

    Update admin.py

--- answers/admin.py
+++ answers/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.http import HttpResponse
+import csv
 
 from .models import (Answer, AnswerInput, AnswerNumberLine, AnswerSelect,
                      AnswerSession, AnswerSort, AssessmentTopicAnswer)
@@ -7,6 +9,21 @@ from .models import (Answer, AnswerInput, AnswerNumberLine, AnswerSelect,
 class AnswerSelectAnswerAdmin(admin.ModelAdmin):
     list_display = ('id', 'topic_answer', 'question_identifier', 'valid')
     search_fields = ('id',)
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
 
     def question_identifier(self, obj):
         return obj.question.identifier
@@ -14,6 +31,21 @@ class AnswerSelectAnswerAdmin(admin.ModelAdmin):
 class AnswerNumberlineAnswerAdmin(admin.ModelAdmin):
     list_display = ('id', 'topic_answer', 'question_identifier', 'valid')
     search_fields = ('id',)
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
 
     def question_identifier(self, obj):
         return obj.question.identifier
@@ -22,10 +54,24 @@ class AnswerNumberlineAnswerAdmin(admin.ModelAdmin):
 class AssessmentTopicAnswerAdmin(admin.ModelAdmin):
     list_display = ('id', 'complete', 'student')
     search_fields = ('id',)
+    actions = ["export_as_csv"]
 
     def student(self, obj):
         return obj.student.first_name
 
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
 
 admin.site.register(AnswerSession, admin.ModelAdmin)
 admin.site.register(AnswerInput, admin.ModelAdmin)
