05d4f1b24ddd8d8440e9d957f30677187a1ea149
Pacôme Rivier <pacome@reliefapplications.org>
2022-05-04 15:38:23 +0200

    pass correctly variables into bash script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+
+OUT=.
+
 set -e
 echo -e "Synchronizing files..."
-rsync -e "ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=yes" -avzr --delete --exclude-from '.gitignore' $OUT/* $CONNECTION:$REMOTE_PATH
+rsync -e "ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes" -avzr --delete --exclude-from '.gitignore' $OUT/* $CONNECTION:$REMOTE_PATH
 #CMD="'""cd $REMOTE_PATH && mv docker-compose.yml.dist docker-compose.yml""'" TODO add when we have dist
 #ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "'"$CMD"'"
 
-echo -e "Copy local.py file..."
-CMD="'""cd $REMOTE_PATH && mv admin/settings/local.py.dist admin/settings/local.py""'"
-ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "'"$CMD"'"
+if [ "$AZURE" = true ]
+then
+  echo -e "Copy local.py file..."
+  CMD="'""cd $REMOTE_PATH && mv admin/settings/local.py.dist admin/settings/local.py""'"
+  ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "'"$CMD"'"
+fi
 
 echo -e "Stopping docker containers..."
 CMD="'""cd $REMOTE_PATH && echo '$SSH_PASS' | sudo -S docker-compose down""'"
